91683a6523cdf0dde25e2838876ea0c4acda7a29
“netoeac” <netoeac.dev@gmail.com>
2022-10-25 08:05:17 -0300

    added images

--- themes/olmo/js/request-form-default.js
+++ themes/olmo/js/request-form-default.js
@@ -2,13 +2,11 @@
 $(document).ready(function() {
 
     "use strict";
-
-    $(".contact-form").submit(function(e) {
+    
+    $(".request-form").submit(function(e) {
         e.preventDefault();
         var name = $(".name");
         var email = $(".email");
-        var subject = $(".subject");
-        var msg = $(".message");
         var flag = false;
         if (name.val() == "") {
             name.closest(".form-control").addClass("error");
@@ -24,36 +22,29 @@ $(document).ready(function() {
             return false;
         } else {
             email.closest(".form-control").removeClass("error").addClass("success");
-        } if (msg.val() == "") {
-            msg.closest(".form-control").addClass("error");
-            msg.focus();
-            flag = false;
-            return false;
-        } else {
-            msg.closest(".form-control").removeClass("error").addClass("success");
             flag = true;
         }
-        var dataString = "name=" + name.val() + "&email=" + email.val() + "&subject=" + subject.val() + "&msg=" + msg.val();
-        $(".loading").fadeIn("slow").html("Loading...");
+        var dataString = "name=" + name.val() + "&email=" + email.val();
+        $(".loading").fadeIn("slow").html("Carregando...");
         $.ajax({
             type: "POST",
             data: dataString,
-            url: "php/contactForm.php",
+            url: "php/requestForm.php",
             cache: false,
             success: function (d) {
                 $(".form-control").removeClass("success");
-                    if(d == 'success') // Message Sent? Show the 'Thank You' message and hide the form
-                        $('.loading').fadeIn('slow').html('<font color="#48af4b">Mail sent Successfullyy.</font>').delay(3000).fadeOut('slow');
-                         else
-                        $('.loading').fadeIn('slow').html('<font color="#ff5607">Mail not sent.</font>').delay(3000).fadeOut('slow');
-                                }
+					if(d == 'success') // Message Sent? Show the 'Thank You' message and hide the form
+						$('.loading').fadeIn('slow').html('<font color="#48af4b">Mensagem enviada.</font>').delay(3000).fadeOut('slow');
+					else
+						$('.loading').fadeIn('slow').html('<font color="#ff5607">Falha ao enviar mensagem.</font>').delay(3000).fadeOut('slow');
+
+								  }
         });
         return false;
     });
     $("#reset").on('click', function() {
         $(".form-control").removeClass("success").removeClass("error");
     });
-    
 })
 
 
